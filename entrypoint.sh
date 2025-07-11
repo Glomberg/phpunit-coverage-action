20f37b28d4c696d6e50d1385932abe05d2bc82e7
Glomberg <bazz@bk.ru>
2025-07-11 11:21:50 +0300

    Action fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y php$PHP_VERSION php$PHP_VERSION-xml php$PHP_VERSION-mbstring
-
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-composer install --no-progress --no-interaction
+composer install
 
-php ./vendor/bin/phpunit --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT
+php $COVERAGE_FORMAT --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT
 
 COVERAGE=$(php -r "\$c = simplexml_load_file('coverage.$COVERAGE_FORMAT'); echo \$c->project->metrics['linecovered'] / \$c->project->metrics['lines'] * 100;")
 

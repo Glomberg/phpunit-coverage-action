4a4eb077100b8199af2805de6e5b18f673bc26af
Glomberg <bazz@bk.ru>
2025-07-11 13:08:20 +0300

    Action fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ composer install
 echo "➡️ $PHPUNIT_COMMAND --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT"
 php $PHPUNIT_COMMAND --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT
 
-COVERAGE=$(php -r "\$c = simplexml_load_file('coverage.$COVERAGE_FORMAT'); echo round(\$c->project->metrics['methodscovered'] / \$c->project->metrics['methods'] * 100, 2);")
+COVERAGE=$(php -r "\$c = simplexml_load_file('coverage.$COVERAGE_FORMAT'); echo round(\$c->project->metrics['coveredmethods'] / \$c->project->metrics['methods'] * 100, 2);")
 
 echo "coverage-percent=$COVERAGE" >> $GITHUB_OUTPUT
 
@@ -25,7 +25,7 @@ echo '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="20">
       <path fill="#4c1" d="M110 0h4v20h-4z"/>
       <rect rx="3" width="150" height="20" fill="url(#a)"/>
       <text x="55" y="15" fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">Methods Coverage</text>
-      <text x="130" y="15" fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">'$COVERAGE'</text>
+      <text x="130" y="15" fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">'$COVERAGE'%</text>
     </svg>' > coverage.svg
 
 echo "➡️ Commit badge"

7708f16f0dbe6ba215d7b947c657e6aeaab8e025
Glomberg <bazz@bk.ru>
2025-07-11 12:15:27 +0300

    Action fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ composer install
 echo "$PHPUNIT_COMMAND --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT"
 php $PHPUNIT_COMMAND --coverage-$COVERAGE_FORMAT=coverage.$COVERAGE_FORMAT
 
-COVERAGE=$(php -r "\$c = simplexml_load_file('coverage.$COVERAGE_FORMAT'); echo \$c->project->metrics['linecovered'] / \$c->project->metrics['lines'] * 100;")
+COVERAGE=$(php -r "\$c = simplexml_load_file('coverage.$COVERAGE_FORMAT'); echo round(\$c->project->metrics['methodscovered'] / \$c->project->metrics['methods'] * 100, 2);")
 
 echo "coverage-percent=$COVERAGE" >> $GITHUB_OUTPUT
 
